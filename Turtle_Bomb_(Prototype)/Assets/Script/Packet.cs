ed05edf7bfc27992b66f8ab7cb6a5895185c548c
RRE1012 <goal1012@hotmail.com>
2018-03-02 06:15:11 +0900

    0302Script_ForServer

---
+++ Turtle_Bomb_(Prototype)/Assets/Script/Packet.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+
+public class CharacterInfoPacket : IPacket<CharInfo>
+{
+    class CharacterInfoSerializer : Serial_Script
+    {
+        public bool Serialize(CharInfo packet)
+        {
+            bool ret = true;
+            ret &= Serialize(packet.size);
+            ret &= Serialize(packet.packet_Type);
+            ret &= Serialize(packet.id);
+            ret &= Serialize(packet.x);
+            ret &= Serialize(packet.z);
+            ret &= Serialize(packet.rotateY);
+            ret &= Serialize(packet.hp);
+            ret &= Serialize(packet.ani_state);
+            return ret;
+        }
+
+        public bool Deserialize(ref CharInfo element)
+        {
+            if (GetDataSize() == 0)
+            {
+                // 데이터가 설정되지 않았다.
+                return false;
+            }
+            bool ret = true;
+            ret &= Deserialize(ref element.size, 1);
+            ret &= Deserialize(ref element.packet_Type, 1);
+            ret &= Deserialize(ref element.id, 1);
+            
+            ret &= Deserialize(ref element.x);
+            
+            ret &= Deserialize(ref element.z);
+            
+            ret &= Deserialize(ref element.rotateY);
+            ret &= Deserialize(ref element.hp);
+          
+            ret &= Deserialize(ref element.ani_state);
+       
+
+            return ret;
+        }
+    }
+
+    CharInfo m_packet;
+
+    //패킷 데이터를 시리얼라이즈 하기 위한 생성자
+    public CharacterInfoPacket(CharInfo data)
+    {
+        m_packet = data;
+    }
+
+    // 바이너리 데이터를 패킷 데이터로 디시리얼라이즈하기 위한 생성자
+    public CharacterInfoPacket(byte[] data)
+    {
+        CharacterInfoSerializer serializer = new CharacterInfoSerializer();
+        serializer.SetDeserializedData(data);
+        serializer.Deserialize(ref m_packet);
+        //serializer.Clear();
+    }
+
+    //패킷 id 얻기
+    public PacketInfo GetPacketId()
+    {
+        return PacketInfo.CharPos;
+    }
+
+    //게임에서 쓸 패킷 데이터 얻음
+    public CharInfo GetPacket()
+    {
+        return m_packet;
+    }
+   
+
+    //송신용 byte[]형 데이터 얻기
+    public byte[] GetData()
+    {
+        CharacterInfoSerializer serializer = new CharacterInfoSerializer();
+        serializer.Serialize(m_packet);
+
+        return serializer.GetSerializedData();
+    }
+}
---
+++ Turtle_Bomb_(Prototype)/Assets/Script/PacketStd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public struct ClientID
+{
+    public byte size;           // 패킷 크기
+    public byte packetType;     // 패킷 id
+    public byte id;             // 클라 id
+    public byte clientType;     // 클라 타입( 0 = PC / 1 = AR )
+    public char[] playerID;     // 입력한 플레이어 id
+    public byte firstClient;    // 먼저 접속한 클라이언트인지 아닌지 ( 0 = 먼저 접속 / 1 = 나중에 접속)
+}
+public struct CharInfo
+{
+    public byte size;
+    public byte packet_Type;
+    public byte id;
+    public float x;
+    public float z;
+    public float rotateY;
+    public float hp;
+    public int ani_state;
+}
+
+public enum PacketInfo
+{
+    ClientID = 1,   // 클라이언트 ID값
+    CharPos,        // 캐릭터 좌표 및 스테이터스
+    EnemyData,      // 적 데이터
+    ItemData,       // 아이템 데이터
+    ObjData,        // 오브젝트 데이터
+    MapData,        // 맵 데이터
+    
+}
