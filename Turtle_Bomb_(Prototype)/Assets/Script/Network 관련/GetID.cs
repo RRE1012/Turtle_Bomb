1c59cf65553c8754deeb4ec0189a01787b0236af
RRE1012 <goal1012@hotmail.com>
2018-07-30 11:30:19 +0900

    0730

---
+++ Turtle_Bomb_(Prototype)/Assets/Script/Network 관련/GetID.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetID : MonoBehaviour {
+    public static GetID instance = null;
+    public InputField[] m_inputfield;
+    string myID = null;
+    string password = null;
+    public GameObject[] m_inputcase;
+    public GameObject[] m_button;
+    public Button[] m_loginButton;
+    public byte login_type;
+    bool inputID = false;
+    bool inputPass=false;
+    // Use this for initialization
+    private void Awake()
+    {
+        instance = this;
+        myID = "test";
+        password = "123";
+        DontDestroyOnLoad(this);
+    }
+    public string GetIDD()
+    {
+        return myID;
+    }
+    public string GETPW()
+    {
+        return password;
+    }
+
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_loginButton[0] != null)
+        {
+            if (login_type == 0)
+            {
+                m_loginButton[0].gameObject.SetActive(false);
+                m_loginButton[1].gameObject.SetActive(false);
+                inputID = false;
+                inputPass = false;
+            }
+            else if (login_type != 0 && inputID && inputPass)
+            {
+                m_loginButton[0].gameObject.SetActive(true);
+                m_loginButton[1].gameObject.SetActive(true);
+            }
+        }
+    }
+    public void openField()
+    {
+        m_inputcase[0].gameObject.SetActive(true);
+        m_button[0].gameObject.SetActive(false);
+        m_button[1].gameObject.SetActive(false);
+        login_type = 1;
+    }
+    public void openField2()
+    {
+        m_inputcase[1].gameObject.SetActive(true);
+        m_button[0].gameObject.SetActive(false);
+        m_button[1].gameObject.SetActive(false);
+        login_type = 2;
+    }
+    public void closeField()
+    {
+        m_inputcase[0].gameObject.SetActive(false);
+        m_inputcase[1].gameObject.SetActive(false);
+        m_button[0].gameObject.SetActive(true);
+        m_button[1].gameObject.SetActive(true);
+        login_type = 0;
+    }
+
+    public void SetId()
+    {
+        
+        myID = m_inputfield[0].text;
+        login_type = 1;
+        Debug.Log("MyID:" +myID);
+    }
+    public void SetId_Create()
+    {
+
+        myID = m_inputfield[2].text;
+        login_type = 2;
+        //Debug.Log(myID);
+    }
+    public void IDInput()
+    {
+        inputID = true;
+        //Debug.Log("changed ID");
+    }
+    public void PasswordInput()
+    {
+        inputPass = true;
+        //Debug.Log("changed PW");
+    }
+    public void SetPassword()
+    {
+        password = m_inputfield[1].text;
+        
+        //Debug.Log(password);
+    }
+    public void SetPasswordJoin()
+    {
+        password = m_inputfield[3].text;
+    }
+    public void KeyBoardOpen()
+    {
+        TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
+        //Debug.Log("Open!");
+    }
+}
---
+++ Turtle_Bomb_(Prototype)/Assets/Script/Network 관련/Performance_Network.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Performance_Network : MonoBehaviour {
+    public Camera m_PlayerView;
+    public Camera m_PerformanceView;
+	public Camera[] m_OtherPlayerView;
+	public Animator[] m_OtherPlayerAnimator;
+    public static Performance_Network instance;
+    public Animator m_CameraAnimator;
+    public bool ani_is_working;
+    // Use this for initialization
+
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start () {
+        ani_is_working = true;
+        if (m_OtherPlayerView.Length>0)
+        {
+            for (int i = 0; i < 4; ++i)
+                m_OtherPlayerView[i].enabled = false;
+        }
+        
+    }
+	public void DeadAnimation(byte id){
+		m_PlayerView.enabled = false;
+		m_OtherPlayerView[id].enabled = true;
+		switch (id) {
+		case 0:
+			m_OtherPlayerAnimator[0].SetTrigger("GameOver");
+			ani_is_working = true;
+			break;
+		case 1:
+			m_OtherPlayerAnimator[1].SetTrigger("GameOver");
+			ani_is_working = true;
+			break;
+		case 2:
+			m_OtherPlayerAnimator[2].SetTrigger("GameOver");
+			ani_is_working = true;
+			break;
+		case 3:
+			m_OtherPlayerAnimator[3].SetTrigger("GameOver");
+			ani_is_working = true;
+			break;
+		
+		}
+		Invoke("CameraSwitch", 9.3f);
+	}
+	void CameraSwitch()
+    {
+        m_PlayerView.enabled = true;
+        m_PerformanceView.enabled = false;
+        if (m_OtherPlayerView.Length > 0)
+        {
+            for (int i = 0; i < 4; ++i)
+                m_OtherPlayerView[i].enabled = false;
+        }
+		ani_is_working = false;
+    }
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public void Intro_Performance(byte id)
+    {
+        m_PlayerView.enabled = false;
+        m_PerformanceView.enabled = true;
+        switch (id)
+        {
+            case 0:
+                m_CameraAnimator.SetTrigger("Intro_Normal");
+                ani_is_working = true;
+                break;
+            case 1:
+                m_CameraAnimator.SetTrigger("Intro_Normal2");
+                ani_is_working = true;
+                break;
+            case 2:
+                m_CameraAnimator.SetTrigger("Intro_Normal3");
+                ani_is_working = true;
+                break;
+            case 3:
+                m_CameraAnimator.SetTrigger("Intro_Normal4");
+                ani_is_working = true;
+                break;
+
+        }
+        Invoke("CameraSwitch", 5.9f);
+    }
+       
+}
